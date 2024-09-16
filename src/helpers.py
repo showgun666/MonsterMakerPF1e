89feaa5d7f21ec8557f22cf5dfcc1461b5ad0d1f
Markus von Wowern <markus.vonwowern@gmail.com>
2024-09-16 21:11:13 +0200

    refactored, imported csv module and simplified generate_list_of_dictionaries function using said module

--- src/helpers.py
+++ src/helpers.py
@@ -1,25 +1,11 @@
 "module for helper functions"
+import csv
 
 def generate_list_of_dictionaries(text_file):
     "Generates a list of dictionaries from given csv table"
     with open(text_file, 'r', encoding='utf-8') as file:
-        row = 0
-        columns = []
-        data = []
-
-        for line in file:
-            if row == 0:
-                columns = line.split(";")
-                row +=1
-                continue
-            dictionary = {}
-            row_values = line.split(";")
-            for i, _ in enumerate(columns):
-                dictionary[columns[i].replace("\n", "")] = row_values[i].replace("\n", "")
-            data.append(dictionary)
-            row += 1
-
-    return data
+        reader = csv.DictReader(file, delimiter=';')
+        return [row for row in reader]
 
 def comma_separated_string_from_list(given_list):
     "Generates a comma separated string"
