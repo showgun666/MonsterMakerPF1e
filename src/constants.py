ae9403be33292436bd2c593517a5c173e7e4a900
Markus von Wowern <markus.vonwowern@gmail.com>
2024-09-17 15:44:51 +0200

    damage dice progression added as a list. two indexes higher on the list would be the next size damage dice

--- src/constants.py
+++ src/constants.py
@@ -25,3 +25,12 @@ WILL = 2
 # SAVE PROGRESSION BOOLEANS
 GOOD = True
 BAD = False
+
+# DAMAGE DICE PROGRESSION LIST, 2 STEPS PER SIZE INCREASE
+DAMAGE_DICE_PROGRESSION = [
+    "1", "1d2", "1d3", "1d4", "1d6", "1d8",
+    "1d10", "2d6", "2d8", "3d6", "3d8", "4d6",
+    "4d8", "6d6", "6d8", "8d6", "8d8", "12d6",
+    "12d8", "16d6", "16d8", "24d6", "24d8",
+    "36d6", "36d8"
+    ]
