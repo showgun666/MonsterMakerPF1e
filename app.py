ab5b02bb7ab8b4bb5216a184b3618d5dc1b005b4
Markus von Wowern <markus.vonwowern@gmail.com>
2024-10-18 14:09:31 +0200

    whole session is put out in plain text in footer for ease of debugging. the monster balancer tests are refactored for smarter use of the monster balancer. the app.py is updated for the monster balancer and can now be used to adjust cr with sliders as well as ac in a decent manner. still no way to reset the session and a lot of work left to do.

--- app.py
+++ app.py
@@ -4,14 +4,99 @@ Flask app for monster creation in Pathfinder 1e.
 To be used as a tool for Dungeon Master's looking to quickly make custom monsters.
 """
 import traceback
-from flask import Flask, render_template
+import os
+import re
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from src.monster_balancer.monster_balancer import get_statistic_column, armor_class_deviated, determine_cr_float, generate_list_of_dictionaries, MONSTER_STATISTICS_BY_CR
 
 app = Flask(__name__)
+app.secret_key = re.sub(r"[^a-z\d]", "", os.path.realpath(__file__))
+
+ORIGIN_INDEX = 1
 
 @app.route("/")
 def main():
     """ Main route """
+    session["ac"] = get_statistic_column("Armor Class")[ORIGIN_INDEX]
+    session["cr"] = ORIGIN_INDEX
+    session["hp"] = get_statistic_column("Hit Points")[ORIGIN_INDEX]
     return render_template("index.html")
+
+@app.route("/settings")
+def settings():
+    """ Settings route """
+    return render_template("settings.html")
+
+@app.route("/creature-designer")
+def creature_designer():
+    """ Creature designer route """
+    return render_template("creature-designer.html")
+
+@app.route("/monster-balancer")
+def monster_balancer():
+    """ Monster balancer route """
+    session["average_ac_for_cr"] = get_statistic_column("Armor Class")[int(session["cr"])]
+    session["ac_deviation"] = armor_class_deviated(session["ac"], session["cr"])
+    session["cr_value_for_ac"] = determine_cr_float("Armor Class", int(session["ac"]))
+    ac_column = get_statistic_column("Armor Class")
+    min_ac = ac_column[0]
+    max_ac = ac_column[-1]
+    hp_column = get_statistic_column("Hit Points")
+    min_hp = hp_column[0]
+    max_hp = hp_column[-1]
+    return render_template(
+        "monster-balancer.html",
+        current_ac = session["ac"],
+        current_cr = session["cr"],
+        current_hp = session["hp"],
+        average_ac_for_cr = session["average_ac_for_cr"],
+        ac_deviation = session["ac_deviation"],
+        min_ac = min_ac,
+        max_ac = max_ac,
+        min_hp = min_hp,
+        max_hp = max_hp,
+        )
+
+@app.route('/update-monster-balancer-ac', methods=['POST'])
+def update_content_monster_balancer_ac():
+    """ AC Update route for monster balancer"""
+    session["ac"] = request.json['acSliderValue']
+    session["ac_deviation"] = armor_class_deviated(session["ac"], session["cr"])
+    session["cr_value_for_ac"] = determine_cr_float("Armor Class", int(session["ac"]))
+    return jsonify(
+        ac_deviation=session["ac_deviation"],
+        cr_value_for_ac=session["cr_value_for_ac"],
+                   )
+
+@app.route('/update-monster-balancer-hp', methods=['POST'])
+def update_content_monster_balancer_hp():
+    """ HP Update route for monster balancer"""
+    session["hp"] = request.json['hpSliderValue']
+
+    return jsonify(
+        expected_cr_for_hp = determine_cr_float("Hit Points", int(session["hp"]))
+                   )
+
+@app.route('/update-monster-balancer-cr', methods=['POST'])
+def update_content_monster_balancer_cr():
+    """ CR Update route for monster balancer"""
+    session["cr"] = request.json['crSliderValue']
+
+    average_ac_for_cr = get_statistic_column("Armor Class")[int(session["cr"])]
+    ac_column = get_statistic_column("Armor Class")
+    average_ac_for_cr = ac_column[int(session["cr"])]
+
+    average_hp_for_cr = get_statistic_column("Hit Points")[int(session["cr"])]
+
+    session["ac_deviation"] = armor_class_deviated(session["ac"], session["cr"])
+    session["cr_value_for_ac"] = determine_cr_float("Armor Class", int(session["ac"]))
+
+    return jsonify(
+        average_ac_for_cr = average_ac_for_cr,
+        ac_deviation = session["ac_deviation"],
+        cr_value_for_ac = session["cr_value_for_ac"],
+        average_hp_for_cr = average_hp_for_cr
+        )
 
 @app.errorhandler(404)
 def page_not_found(e):
--- tests/test_monster_balancer.py
+++ tests/test_monster_balancer.py
@@ -4,6 +4,7 @@
 
 import unittest
 from src.monster_balancer.monster_balancer import determine_cr_float
+from src.monster_balancer.monster_balancer import armor_class_deviated
 
 class TestMonsterBalancer(unittest.TestCase):
     "Tests for monster balancer module"
@@ -22,11 +23,13 @@ class TestMonsterBalancer(unittest.TestCase):
             determine_cr_float("Hit Points", 33), # 3.3
             determine_cr_float("Hit Points", 370), # 20
             determine_cr_float("Primary Ability DC", 27), # 20
+            determine_cr_float("Armor Class", 22), # 20
         ]
         self.assertEqual(values[0], 0)
         self.assertEqual(values[1], 3.3)
         self.assertEqual(values[2], 20)
         self.assertEqual(values[3], 20)
+        self.assertEqual(values[4], 8.5)
 
     def test_determine_cr_float_low_value_raises_value_error(self):
         """
@@ -37,3 +40,28 @@ class TestMonsterBalancer(unittest.TestCase):
             determine_cr_float("Hit Points", 8)
             determine_cr_float("Hit Points", 0)
             determine_cr_float("Primary Ability DC", 36)
+
+    def test_armor_class_deviation(self):
+        """
+        correctly determines if ac value deviates too much.
+        """
+        high_low1 = armor_class_deviated(22, 3)
+        high_low2 = armor_class_deviated(12, 3)
+        high_low3 = armor_class_deviated(11, 7)
+        high_low4 = armor_class_deviated(18, 3)
+        high_low5 = armor_class_deviated(47, 30)
+
+        # Too high
+        self.assertEqual(high_low1, 1)
+
+        # Low but OK
+        self.assertEqual(high_low2, 0)
+
+        # Too low
+        self.assertEqual(high_low3, -1)
+
+        # High but ok
+        self.assertEqual(high_low4, 0)
+
+        # Max value OK
+        self.assertEqual(high_low5, 0)
